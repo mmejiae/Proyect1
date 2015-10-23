25aa7b446574a0f7cb365720d4f65bf65463d57d
Mariana <nanitamejes@hotmail.com>
2015-10-23 17:46:17 -0500

    mas cosas

--- app/src/main/java/com/example/marcela/proyecto1/Vista2Activity.java
+++ app/src/main/java/com/example/marcela/proyecto1/Vista2Activity.java
@@ -1,13 +1,37 @@
 package com.example.marcela.proyecto1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class Vista2Activity extends AppCompatActivity {
+public class Vista2Activity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btn_eliminar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vista2);
+
+        btn_eliminar = (Button) findViewById(R.id.btn_eliminar);
+        btn_eliminar.setOnClickListener(this);
+
+
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
+
+
+
+
+
+
+
